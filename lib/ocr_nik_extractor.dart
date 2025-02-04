e3c45ac8acb6243854505f267ef92d8220f5cede
altantheprodigy <altanpoke9@gmail.com>
2025-02-04 14:27:29 +0700

    extractor

---
+++ lib/ocr_nik_extractor.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+class OcrNikExtractor {
+  Future<String> extractNik({File? imageFile, Uint8List? imageBytes, int? width, int? height}) async {
+    try {
+      final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+
+      InputImage inputImage;
+      if (imageFile != null) {
+        inputImage = InputImage.fromFile(imageFile);
+      } else if (imageBytes != null && width != null && height != null) {
+        inputImage = InputImage.fromBytes(
+          bytes: imageBytes,
+          metadata: InputImageMetadata(
+            size: Size(width.toDouble(), height.toDouble()),
+            rotation: InputImageRotation.rotation0deg,
+            format: InputImageFormat.nv21,
+            bytesPerRow: width,
+          ),
+        );
+      } else {
+        throw Exception('Either imageFile or imageBytes must be provided');
+      }
+
+      final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+
+      String nik = _extractNikFromText(recognizedText.text);
+
+      textRecognizer.close();
+
+      if (nik.isNotEmpty) {
+        return nik;
+      } else {
+        return "NIK tidak terdeteksi";
+      }
+
+    } catch (e) {
+      throw Exception('Terjadi kesalahan: $e');
+    }
+  }
+
+  String _extractNikFromText(String text) {
+    final RegExp nikRegExp = RegExp(r'\b\d{16}\b');
+    final Match? match = nikRegExp.firstMatch(text);
+    return match?.group(0) ?? '';
+  }
+}
